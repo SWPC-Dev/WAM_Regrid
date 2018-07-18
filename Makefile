c041db1709bbcdf53f121fb77cc8ddccaaff38d3
Joseph Schoonover <joseph.schoonover@noaa.gov>
2018-07-18 11:02:03 -0600

    Fixed bug in interpolation.

--- Makefile
+++ Makefile
@@ -1,11 +1,22 @@
 
 
 FC=gfortran
-OPT= -ffree-line-length-none -O0 -g -fbacktrace
+OPT= -ffree-line-length-none -O3
 
-LIB=-L/home/joe/spack/opt/spack/linux-ubuntu16.04-x86_64/gcc-5.4.0/netcdf-4.6.1-ohldsascx6453mdolilxykvhnufhl3q3/lib -lnetcdf -L/home/joe/spack/opt/spack/linux-ubuntu16.04-x86_64/gcc-5.4.0/netcdf-fortran-4.4.4-llqdlrjpkkf7eatofynzx3djwbvl2ubt/lib -lnetcdff
+LIB=-L/home/joe/spack/opt/spack/linux-ubuntu16.04-x86_64/gcc-5.4.0/netcdf-fortran-4.4.4-llqdlrjpkkf7eatofynzx3djwbvl2ubt/lib -lnetcdff 
+LIB+= -L/home/joe/spack/opt/spack/linux-ubuntu16.04-x86_64/gcc-5.4.0/netcdf-4.6.1-ohldsascx6453mdolilxykvhnufhl3q3/lib -lnetcdf
 
 INC=-I/home/joe/spack/opt/spack/linux-ubuntu16.04-x86_64/gcc-5.4.0/netcdf-fortran-4.4.4-qbxkckbzlpyq7xadda3z3mbbw4jkfs22/include
 
-regrid: WAM_Reinterpolate.f90
-	${FC} ${OPT} WAM_Reinterpolate.f90 ${LIB} ${INC} -o $@
+.PHONY : clean \
+         regrid
+
+regrid: WAM_Reinterpolate.o
+	${FC} ${OPT} WAM_Reinterpolate.o ${LIB} ${INC} -o $@
+
+WAM_Reinterpolate.o: WAM_Reinterpolate.f90
+	${FC} ${OPT} -c WAM_Reinterpolate.f90 ${LIB} ${INC} -o $@
+
+
+clean: 
+	rm *.o *.mod regrid
--- WAM_Reinterpolate.f90
+++ WAM_Reinterpolate.f90
@@ -6,10 +6,9 @@ IMPLICIT NONE
 
   ! /////////////////////////////////////////////////// !
 
-  INTEGER, PARAMETER :: n_regridded_levels = 40
-  REAL(8), PARAMETER :: z0_regridded       = 0.0D0
-  REAL(8), PARAMETER :: zM_regridded       = 400.0D0
-  REAL(8), PARAMETER :: fillValue          = -9999.99D0
+  INTEGER, PARAMETER :: n_regridded_levels = 20
+  REAL(8), PARAMETER :: z0_regridded       = 100.0D0
+  REAL(8), PARAMETER :: zM_regridded       = 300.0D0
 
 
   ! /////////////////////////////////////////////////// !
@@ -18,8 +17,8 @@ IMPLICIT NONE
   INTEGER :: i, j, k 
   INTEGER :: n_time, n_lat, n_lon, n_pressure_levels
 
-  REAL(4) :: t1, t2
   REAL(8) :: z_regridded(1:n_regridded_levels)
+  REAL(8) :: z_regridded_log(1:n_regridded_levels)
 
   REAL(8), ALLOCATABLE :: A(:,:)
   REAL(8), ALLOCATABLE :: temperature(:,:,:,:)
@@ -47,24 +46,19 @@ IMPLICIT NONE
 
     CALL Reorder_Pressure_Arrays( )
 
-    !$OMP PARALLEL DO PRIVATE( A )
+    !$OMP PARALLEL DO
     DO k = 1, n_time
       DO j = 1, n_lat
         DO i = 1, n_lon
 
-          CALL CPU_TIME( t1 )
-          CALL Generate_Interpolation_Matrix( z_pressure_reordered(:,i,j,k), z_regridded, A, n_pressure_levels )
-  
-          CALL Apply_Interpolation_Matrix( temperature_reordered(:,i,j,k), regridded_temperature_reordered(:,i,j,k), A )
-          CALL Apply_Interpolation_Matrix( density_reordered(:,i,j,k), regridded_density_reordered(:,i,j,k), A )
-          CALL CPU_TIME( t2 )
+          CALL Interpolate( z_pressure_reordered(1:n_pressure_levels,i,j,k), z_regridded, temperature_reordered(1:n_pressure_levels,i,j,k), regridded_temperature_reordered(1:n_regridded_levels,i,j,k) )
+          CALL Interpolate( z_pressure_reordered(1:n_pressure_levels,i,j,k), z_regridded, density_reordered(1:n_pressure_levels,i,j,k), regridded_density_reordered(1:n_regridded_levels,i,j,k) )
 
-          PRINT*, ' Interpolate Time ', t2 - t1
 
         ENDDO
       ENDDO
     ENDDO
-    !$OMP PARALLEL ENDDO
+!    !$OMP PARALLEL ENDDO
 
     CALL Reorder_Regridded_Arrays( )
 
@@ -212,6 +206,7 @@ CONTAINS
 
      DO i = 1, n_regridded_levels
        z_regridded(i) = z0_regridded + ( zM_regridded - z0_regridded )/REAL( n_regridded_levels-1, 8 ) * REAL(i-1)
+       z_regridded_log(i) = log( z_regridded(i) )
      ENDDO
 
 
@@ -268,13 +263,17 @@ CONTAINS
 
     ! Local
     INTEGER :: i, j, k 
+    REAL(8) :: max_z
 
+
+    max_z = MAXVAL( z_pressure )
     DO k = 1, n_time
       DO j = 1, n_lat
         DO i = 1, n_lon
 
           temperature_reordered(1:n_pressure_levels,i,j,k) = temperature(i,j,1:n_pressure_levels,k)
-          z_pressure_reordered(1:n_pressure_levels,i,j,k)  = z_pressure(i,j,1:n_pressure_levels,k)
+          
+          z_pressure_reordered(1:n_pressure_levels,i,j,k)  = z_pressure(i,j,1:n_pressure_levels,k) 
           density_reordered(1:n_pressure_levels,i,j,k)     = density(i,j,1:n_pressure_levels,k)
 
         ENDDO
@@ -325,9 +324,6 @@ CONTAINS
 
       CALL Allocate_Arrays( )
 
-      CALL Check( nf90_inq_varid( ncid, "t", varid ) )
-      CALL Check( nf90_get_var( ncid, varid, temperature ) )
-
       CALL Check( nf90_inq_varid( ncid, "time", varid ) )
       CALL Check( nf90_get_var( ncid, varid, time ) )
 
@@ -343,6 +339,9 @@ CONTAINS
       CALL Check( nf90_inq_varid( ncid, "den", varid ) )
       CALL Check( nf90_get_var( ncid, varid, density ) )
 
+      CALL Check( nf90_inq_varid( ncid, "t", varid ) )
+      CALL Check( nf90_get_var( ncid, varid, temperature ) )
+
       CALL Check( nf90_close( ncid ) )
 
   END SUBROUTINE Read_NetCDF
@@ -354,8 +353,13 @@ CONTAINS
     INTEGER :: x_dimid, y_dimid, z_dimid, time_dimid
     INTEGER :: x_varid, y_varid, z_varid, time_varid
     INTEGER :: temperature_varid, density_varid
+    INTEGER :: recStart(1:4), recCount(1:4)
 
-      CALL Check( nf90_create( filename, NF90_CLOBBER, ncid))
+
+      recStart = (/ 1, 1, 1, 1 /)
+      recCount = (/ n_lon, n_lat, n_regridded_levels, n_time /)
+
+      CALL Check( nf90_create( TRIM(filename), NF90_NETCDF4, ncid))
 
       CALL Check( nf90_def_dim( ncid, "Z", n_regridded_levels, z_dimid ) ) 
       CALL Check( nf90_def_dim( ncid, "longitude", n_lon, x_dimid ) ) 
@@ -365,26 +369,18 @@ CONTAINS
       CALL Check( nf90_def_var( ncid, "Z", NF90_DOUBLE, z_dimid, z_varid ) )
       CALL Check( nf90_put_att( ncid, z_varid, "long_name", "Radial Distance" ) )
       CALL Check( nf90_put_att( ncid, z_varid, "units", "km" ) )
-      CALL Check( nf90_put_att( ncid, z_varid, "_FillValue", fillValue) )
-      CALL Check( nf90_put_att( ncid, z_varid, "missing_value", fillValue) )
 
       CALL Check( nf90_def_var( ncid, "longitude", NF90_DOUBLE, x_dimid, x_varid ) )
       CALL Check( nf90_put_att( ncid, x_varid, "long_name", "Geodesic Longitude" ) )
       CALL Check( nf90_put_att( ncid, x_varid, "units", "degrees east" ) )
-      CALL Check( nf90_put_att( ncid, x_varid, "_FillValue", fillValue) )
-      CALL Check( nf90_put_att( ncid, x_varid, "missing_value", fillValue) )
 
       CALL Check( nf90_def_var( ncid, "latitude", NF90_DOUBLE, y_dimid, y_varid ) )
       CALL Check( nf90_put_att( ncid, y_varid, "long_name", "Geodesic Latitude" ) )
       CALL Check( nf90_put_att( ncid, y_varid, "units", "degrees north" ) )
-      CALL Check( nf90_put_att( ncid, y_varid, "_FillValue", fillValue) )
-      CALL Check( nf90_put_att( ncid, y_varid, "missing_value", fillValue) )
 
       CALL Check( nf90_def_var( ncid, "T", NF90_INT, time_dimid, time_varid ) )
       CALL Check( nf90_put_att( ncid, time_varid, "long_name", "minutes since an unknown time" ) )
       CALL Check( nf90_put_att( ncid, time_varid, "units", "minutes" ) )
-      CALL Check( nf90_put_att( ncid, time_varid, "_FillValue", fillValue) )
-      CALL Check( nf90_put_att( ncid, time_varid, "missing_value", fillValue) )
 
       CALL Check( nf90_def_var( ncid, "temperature", NF90_DOUBLE, (/ x_dimid, y_dimid, z_dimid, time_dimid /) , temperature_varid ) )
       CALL Check( nf90_put_att( ncid, temperature_varid, "long_name", "Thermosphere Temperature" ) )
@@ -400,8 +396,8 @@ CONTAINS
       CALL Check( nf90_put_var( ncid, y_varid, lat ) )
       CALL Check( nf90_put_var( ncid, z_varid, z_regridded + 6371.0D0 ) )
       CALL Check( nf90_put_var( ncid, time_varid, time ) )
-      CALL Check( nf90_put_var( ncid, temperature_varid, regridded_temperature ) )
-      CALL Check( nf90_put_var( ncid, density_varid, regridded_density ) )
+      CALL Check( nf90_put_var( ncid, temperature_varid, regridded_temperature, recStart, recCount ) )
+      CALL Check( nf90_put_var( ncid, density_varid, regridded_density, recStart, recCount ) )
 
       CALL Check( nf90_close( ncid ) )
 
@@ -409,48 +405,32 @@ CONTAINS
 
   END SUBROUTINE Write_NetCDF
 
-  SUBROUTINE Generate_Interpolation_Matrix( z_pressure, z_regridded, interp_matrix, n_pressure_levels )
-    INTEGER, INTENT(in)  :: n_pressure_levels
-    REAL(8), INTENT(in)  :: z_pressure(1:n_pressure_levels)
-    REAL(8), INTENT(in)  :: z_regridded(1:n_regridded_levels)
-    REAL(8), INTENT(out) :: interp_matrix(1:n_pressure_levels,1:n_regridded_levels) !! Or you could do this as a tridiagonal storage...
+  SUBROUTINE Interpolate( z_pressure, z_regridded, f_pressure, f_regridded )
+    REAL(8), INTENT(in)    :: f_pressure(1:n_pressure_levels)
+    REAL(8), INTENT(in)    :: z_pressure(1:n_pressure_levels)
+    REAL(8), INTENT(in)    :: z_regridded(1:n_regridded_levels)
+    REAL(8), INTENT(inout) :: f_regridded(1:n_regridded_levels)
     ! Local
-    INTEGER :: row
-
-      DO row = 0, n_regridded_levels
-
-        interp_matrix(1:n_pressure_levels,row) = Hat_Function( z_pressure, z_regridded(row), n_pressure_levels )
-
-      ENDDO
-    
-
-  END SUBROUTINE Generate_Interpolation_Matrix
-
-  SUBROUTINE Apply_Interpolation_Matrix( f_pressure, f_regridded, interp_matrix )
-    REAL(8), INTENT(in)  :: f_pressure(1:n_pressure_levels)
-    REAL(8), INTENT(out) :: f_regridded(1:n_pressure_levels)
-    REAL(8), INTENT(in)  :: interp_matrix(1:n_regridded_levels,1:n_pressure_levels) !! Or you could do this as a tridiagonal storage...
-    ! Local
+    REAL(8) :: interp(1:n_pressure_levels)
     INTEGER :: row, col
     REAL(8) :: f_new
 
       DO row = 1, n_regridded_levels
 
+        interp(1:n_pressure_levels) = Hat_Function( z_pressure, z_regridded(row), n_pressure_levels )
         f_new = 0.0D0
 
         DO col = 1, n_pressure_levels
      
-          f_new = f_new + interp_matrix(col,row)*f_pressure(col)
+          f_new = f_new + interp(col)*f_pressure(col)
 
         ENDDO
 
         f_regridded(row) = f_new
 
       ENDDO
-       
 
-
-  END SUBROUTINE Apply_Interpolation_Matrix
+  END SUBROUTINE Interpolate
 
   FUNCTION Hat_Function( z_pressure, z_interp, n_pressure_levels ) RESULT( phi )
     INTEGER :: n_pressure_levels
@@ -461,7 +441,7 @@ CONTAINS
     INTEGER :: i
 
       i = 1
-      IF( z_interp >= z_pressure(i) .AND. z_interp <= z_pressure(i+1) )THEN
+      IF( z_interp >= z_pressure(i) .AND. z_interp < z_pressure(i+1) )THEN
   
         phi(i) = ( z_interp - z_pressure(i+1) )/( z_pressure(i) - z_pressure(i+1) )
   
@@ -473,11 +453,11 @@ CONTAINS
 
       DO i = 2, n_pressure_levels-1
 
-        IF( z_interp >= z_pressure(i-1) .AND. z_interp < z_pressure(i) )THEN
+        IF( z_interp > z_pressure(i-1) .AND. z_interp <= z_pressure(i) )THEN
   
           phi(i) = ( z_interp - z_pressure(i-1) )/( z_pressure(i) - z_pressure(i-1) )
   
-        ELSEIF( z_interp >= z_pressure(i) .AND. z_interp <= z_pressure(i+1) )THEN
+        ELSEIF( z_interp >= z_pressure(i) .AND. z_interp < z_pressure(i+1) )THEN
   
           phi(i) = ( z_interp - z_pressure(i+1) )/( z_pressure(i) - z_pressure(i+1) )
   
@@ -490,7 +470,7 @@ CONTAINS
       ENDDO
 
       i = n_pressure_levels
-      IF( z_interp >= z_pressure(i-1) .AND. z_interp <= z_pressure(i) )THEN
+      IF( z_interp > z_pressure(i-1) .AND. z_interp <= z_pressure(i) )THEN
 
         phi(i) = ( z_interp - z_pressure(i-1) )/( z_pressure(i) - z_pressure(i-1) )
 
